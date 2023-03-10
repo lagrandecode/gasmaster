734de7c4e450ec0a59de7899c9624efc47796b71
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-10 22:46:18 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductModel(models.Model):
+    product_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=250)
+    discount = models.PositiveIntegerField()
+    selling_price = models.PositiveIntegerField()
+    photo = models.ImageField()
+    kg = models.PositiveIntegerField()
