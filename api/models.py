98399698df69b792a08d4382350c8c4df1fb967b
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-12 23:30:43 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class ProductModel(models.Model):
+class Product(models.Model):
     product_name = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
     discount = models.PositiveIntegerField()
-    selling_price = models.PositiveIntegerField()
-    photo = models.ImageField()
+    selling_price = models.PositiveIntegerField(null=True,blank=True)
+    photo = models.ImageField(upload_to='productimages/',blank=True,null=True)
     kg = models.PositiveIntegerField()
 
     def __str__(self):
         return self.product_name
+
 
 
 #This class will show the news in the carousel (blog)
@@ -22,9 +23,44 @@ class GasNews(models.Model):
     news_link = models.URLField()
     created_date = models.DateField(auto_now_add=True)
 
+
+
+
 class Customer(models.Model):
+    name = models.CharField(max_length=250)
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pics = models.ImageField(upload_to='images/',null=True,blank=True)
+    address = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=200, null=False)
+
+
+
+
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Order Confirmed','Order Confirmed'),
+        ('Out of Delivery','Out of Delivery'),
+        ('Delivered','Delivered'),
+    )
+    delivery_status = models.CharField(max_length=50,null=True,choices=STATUS)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE,null=True)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True)
+    email = models.CharField(max_length=50,null=True)
+    address = models.CharField(max_length=500,null=True)
+    mobile = models.CharField(max_length=20,null=True)
+    order_date= models.DateField(auto_now_add=True,null=True)
+
+
+
+class Feedback(models.Model):
+    name=models.CharField(max_length=40)
+    feedback=models.CharField(max_length=500)
+    date= models.DateField(auto_now_add=True,null=True)
+
+
 
 
 
