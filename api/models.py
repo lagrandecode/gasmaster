e5c87f3986728bf0cc2982f7c1273a0b0b86a767
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-12 08:05:26 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,9 +11,18 @@ class ProductModel(models.Model):
     photo = models.ImageField()
     kg = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.product_name
 
-#This class will show the news in the carousel 
+
+#This class will show the news in the carousel (blog)
 class GasNews(models.Model):
     news_image = models.ImageField()
     news_desc = models.CharField(max_length=1000)
     news_link = models.URLField()
+    created_date = models.DateField(auto_now_add=True)
+
+class Customer(models.Model):
+
+
+
