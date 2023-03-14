aa8664581fba7b55fec3103f273e9640aea1f56c
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-14 19:57:39 +0100

    "commit"

--- api/serializers.py
+++ api/serializers.py
@@ -3,5 +3,21 @@ from .models import *
 
 class ProductSerializers(serializers.ModelSerializer):
     class Meta:
-        models = Product
+        model = Product
+        fields = '__all__'
+
+class GasNewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GasNews
+        fields = '__all__'
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Order
         fields = '__all__'
