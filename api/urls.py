81aa7f1b105b1cbac9b22ea0ae5611660ee71e8d
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-17 16:09:17 +0100

    "commit"

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,15 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls import static
 
 
 urlpatterns = [
-    path('',views.api_list)
+    path('',views.api_list),
+    path('',views.product_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/views.py
+++ api/views.py
@@ -12,7 +12,7 @@ from .models import *
 # Create your views here.
 
 @api_view(['GET','POST'])
-def api_list(request):
+def product_list(request):
     if request.method == 'GET':
         api = Product.objects.all()
         serializers = ProductSerializers(api,many=True)
@@ -26,4 +26,22 @@ def api_list(request):
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
+@api_view(['GET','PUT','DELETE'])
+def product_view(request,pk):
+    try:
+        api = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        serializers = ProductSerializers(api)
+        return Response(serializers.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializers = ProductSerializers(api,data=data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        api.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
