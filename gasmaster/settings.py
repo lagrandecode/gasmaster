a77915b1b302b75aa84238eb7f405bf3eac3a280
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-20 19:43:58 +0100

    "commit"

--- gasmaster/settings.py
+++ gasmaster/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-from decouple import Config
+from decouple import con
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
