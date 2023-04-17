b9604d07eb67c2973c1c4052134363fb7034f91e
lagrandecode <seunogunmolufirst1@gmail.com>
2023-04-17 22:15:53 +0100

    "commit"

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from . import views
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 
 urlpatterns = [
