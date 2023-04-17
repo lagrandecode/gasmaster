34ab3b4ef4a67165e3785b8fbbab158dac16beca
lagrandecode <seunogunmolufirst1@gmail.com>
2023-04-17 22:38:05 +0100

    "commit"

--- api/models.py
+++ api/models.py
@@ -55,6 +55,7 @@ class Order(models.Model):
 
 
 class Feedback(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,null=True)
     name=models.CharField(max_length=40)
     feedback=models.CharField(max_length=500)
     date= models.DateField(auto_now_add=True,null=True)
