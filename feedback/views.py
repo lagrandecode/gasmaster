e5a11860f764571e651f00527d6cd7e3bbbfa6e2
lagrandecode <seunogunmolufirst1@gmail.com>
2023-05-04 16:28:45 +0100

    "commit"

--- feedback/views.py
+++ feedback/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework 
+from rest_framework import status 
 
-=======
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import serializers
 from rest_framework import generics
-from . import serializers
-from .models import Feedback
->>>>>>> 916fbbdbb0a9507d657e7ad84134b2c8f0442de3
 # Create your views here.
 
 
